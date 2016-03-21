e305df592bfde2f3334d270d03aa3aa5a75c6a57
aruanruan@vip.sina.com <aruanruan@vip.sina.com>
2016-03-21 22:02:00 +0800

    bug fix

--- src/java/main/org/apache/zookeeper/server/quorum/QuorumStateListenerProxy.java
+++ src/java/main/org/apache/zookeeper/server/quorum/QuorumStateListenerProxy.java
@@ -1,10 +1,28 @@
 package org.apache.zookeeper.server.quorum;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
+/**
+ * 
+ * @author aruan
+ * email: aruanruan@vip.sina.com
+ * qq: 33224696
+ * 2016年3月21日 下午9:26:56
+ *
+ */
 public class QuorumStateListenerProxy implements QuorumStateListener {
 	private QuorumStateListener chain;
-	private QuorumStateListener proxy;
+	private List<QuorumStateListener> proxies;
+	
+	public QuorumStateListenerProxy(){
+		
+	}
+	
+	public QuorumStateListenerProxy(QuorumStateListener chain){
+		this.chain = chain;
+	}
 	
 	public QuorumStateListener getChain() {
 		return chain;
@@ -14,18 +32,51 @@ public class QuorumStateListenerProxy implements QuorumStateListener {
 		this.chain = chain;
 	}
 
-	public QuorumStateListener getProxy() {
-		return proxy;
+	public QuorumStateListener add(QuorumStateListener listener) {
+		if(listener != null){
+			if(proxies == null){
+				proxies = new ArrayList<QuorumStateListener>();
+			}
+			proxies.add(listener);
+		}
+		return this;
+	}
+	
+	public QuorumStateListener add(QuorumStateListener... listeners) {
+		if(listeners != null){
+			if(proxies == null){
+				proxies = new ArrayList<QuorumStateListener>();
+			}
+			if(listeners != null && listeners.length > 0){
+				for(QuorumStateListener listener : listeners){
+					if(listener != null) proxies.add(listener);
+				}
+			}
+		}
+		return this;
+	}
+	
+	public QuorumStateListener add(List<QuorumStateListener> listeners) {
+		if(listeners != null){
+			if(proxies == null){
+				proxies = new ArrayList<QuorumStateListener>();
+			}
+			if(listeners != null && listeners.size() > 0){
+				for(QuorumStateListener listener : listeners){
+					if(listener != null) proxies.add(listener);
+				}
+			}
+		}
+		return this;
 	}
 
-	public void setProxy(QuorumStateListener proxy) {
-		this.proxy = proxy;
-	}
-
+	 
 	@Override
 	public void startup(Properties zkProp, QuorumPeerConfig config) {
-		if(proxy != null){
-			proxy.startup(zkProp, config);
+		if(proxies != null && !proxies.isEmpty()){
+			for(QuorumStateListener listener : proxies){
+				listener.startup(zkProp, config);
+			}
 		}
 		if(chain != null){
 			chain.startup(zkProp, config);
@@ -34,8 +85,10 @@ public class QuorumStateListenerProxy implements QuorumStateListener {
 
 	@Override
 	public void shutdown() {
-		if(proxy != null){
-			proxy.shutdown();
+		if(proxies != null && !proxies.isEmpty()){
+			for(QuorumStateListener listener : proxies){
+				listener.shutdown();
+			}
 		}
 		if(chain != null){
 			chain.shutdown();
@@ -44,8 +97,10 @@ public class QuorumStateListenerProxy implements QuorumStateListener {
 
 	@Override
 	public void stateChanged(State state, QuorumPeer peer) {
-		if(proxy != null){
-			proxy.stateChanged(state, peer);
+		if(proxies != null && !proxies.isEmpty()){
+			for(QuorumStateListener listener : proxies){
+				listener.stateChanged(state, peer);
+			}
 		}
 		if(chain != null){
 			chain.stateChanged(state, peer);
